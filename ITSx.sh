8a8616beceb81be894f2c760217bdb7aaa534235
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-09-05 15:17:46 +0000

    modification

--- ITSx.sh
+++ ITSx.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
-#SBATCH --job-name="ITSX"
+#SBATCH --job-name="2018_2019"
 #SBATCH --cpus-per-task=128
 #SBATCH --nodes=1
-#SBATCH --mem=32G
+#SBATCH --mem=8G
 #SBATCH --partition amd
-#SBATCH --time=150:00:00
+#SBATCH --time=180:00:00
 
 
 # Create the main output directory if it doesn't exist
 mkdir -p itsx_out
 
-# Loop over all FASTA files in the current directory
-for fasta in *.fasta
-do
-# Create a subdirectory for the current file
-subdir="itsx_out/${fasta%.*}"
-mkdir -p "$subdir"
+for fasta in *.fasta; do
+    # Create a subdirectory for the current file
+    subdir="itsx_out/${fasta%.*}"
+    mkdir -p "$subdir"
 
-# Run ITSx with the specified settings and save the output in the subdirectory
-ITSx \
---cpu 128 \
--i "$fasta" \
--o "$subdir/${fasta%.*}" \
--nhmmer T \
---complement T \
---only_full T \
--domains 2 \
--score 0 \
---partial 50 \
---not_found F \
---graphical F \
---preserve T \
---summary F \
-
+    # Run ITSx with the specified settings and save the output in the subdirectory
+    ITSx \
+    --cpu 128 \
+    -i "$fasta" \
+    -o "$subdir/${fasta%.*}" \
+    -nhmmer T \
+    -E 1e-2 \
+    --complement T \
+    --only_full T \
+    -domains 2 \
+    -score 0 \
+    --partial 50 \
+    --save_regions ITS2 \
+    --not_found F \
+    --graphical F \
+    --preserve T \
+    --summary F 
 done

eb65e169230b99edbcac7b28d500238934daa9fc
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-09-05 08:54:17 +0000

    script for blast by unite database

---
+++ blast.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name="blast"
+#SBATCH --cpus-per-task=128
+#SBATCH --nodes=1
+#SBATCH --mem=16G
+#SBATCH --partition amd
+#SBATCH --time=30:00:00
+
+blastn -query /gpfs/space/home/alihakim/analysis/2014_cluster/OTUs.fasta \
+-db /gpfs/space/home/alihakim/analysis/databse/UNITE \
+-word_size 7 \
+-num_threads 128 \
+-outfmt "6  delim=; qseqid stitle qlen slen qstart qend sstart send evalue length nident mismatch gapopen gaps sstrand qcovs pident" \
+-evalue 0.001 \
+-strand both \
+-max_target_seqs 10 \
+-max_hsps 2 \
+-dust no \
+-soft_masking true \
+-penalty -1 \
+-reward 1 \
+-gapopen 1 \
+-gapextend 2 \
+-out blast_results.txt

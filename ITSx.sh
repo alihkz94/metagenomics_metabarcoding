ab084392d8f793703b7602bf7059bd8849964865
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-09-05 09:29:35 +0000

    update the script

--- ITSx.sh
+++ ITSx.sh
@@ -13,19 +13,24 @@ mkdir -p itsx_out
 # Loop over all FASTA files in the current directory
 for fasta in *.fasta
 do
-  # Create a subdirectory for the current file
-  subdir="itsx_out/${fasta%.*}"
-  mkdir -p "$subdir"
+# Create a subdirectory for the current file
+subdir="itsx_out/${fasta%.*}"
+mkdir -p "$subdir"
 
-  # Run ITSx with the specified settings and save the output in the subdirectory
-  ITSx \
-  --cpu 128 \
-  -i "$fasta" \
-  -o "$subdir/${fasta%.*}" \
-  -nhmmer T \
-  --complement T \
-  --only_full T \
-  -domains 2 \
-  -score 0 \
-  --partial 50 
+# Run ITSx with the specified settings and save the output in the subdirectory
+ITSx \
+--cpu 128 \
+-i "$fasta" \
+-o "$subdir/${fasta%.*}" \
+-nhmmer T \
+--complement T \
+--only_full T \
+-domains 2 \
+-score 0 \
+--partial 50 \
+--not_found F \
+--graphical F \
+--preserve T \
+--summary F \
+
 done

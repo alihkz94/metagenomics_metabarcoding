b2a70f1258d042fce5cbaaaedfe23ef66edc8f40
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2024-08-07 15:46:45 +0300

    mafft analysis script

---
+++ mafft.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mafft --thread 8 --auto /home/ali/Documents/simulated_data/analysis/final_results/phylogeny/uchime_chimeras.fasta > /home/ali/Documents/simulated_data/analysis/final_results/phylogeny/aligned_uchime.fasta

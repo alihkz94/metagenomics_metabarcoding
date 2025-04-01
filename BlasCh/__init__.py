e2634c598666a2c4e03b3ed51f70a34338b5405e
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2025-04-01 11:44:58 +0000

    Blasch as a python package added

---
+++ BlasCh/__init__.py
@@ -0,0 +1,7 @@
+"""
+BlasCh - Chimera Detection and Recovery for Metabarcoding and Environmental DNA (eDNA)
+"""
+
+from .main import main, run_blast_analysis, process_blast_xml_results
+
+__version__ = "0.1.0"
